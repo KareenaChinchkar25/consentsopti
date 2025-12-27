686acf89e624688f073cbca76b11aadf98a70637
KareenaChinchkar25 <149800095+KareenaChinchkar25@users.noreply.github.com>
2025-12-27 12:58:07 +0530

    Change API base URL for the service

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_BASE_URL = "https://consent-backend.onrender.com";
+const API_BASE_URL = "https://consent-flow-optimizer.onrender.com";
 
 
 const api = axios.create({
